3a959b6fece8728b45a2654a72c509de1cba084e
Retr0MrWave <retr0.mrwave@gmail.com>
2019-12-20 08:05:28 +0000

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <fstream>
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
+	// initialization
+	ifstream fin ("program.oa");
+	string command;
+
+	// main loop
+	while (!fin.eof())
+	{
+		//fetch
+		fin >> command;
+		//execute
+		
+	}
+
 	return 0;
 }
