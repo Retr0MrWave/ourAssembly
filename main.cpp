fd8654bc5dfdb0a82d4893d7d371293c27a55c6b
YaroslavQLT <yaroslav_ibragimov@ukr.net>
2019-12-20 18:15:10 +0000

    Added SET regex. Fixed source file reading. Added SET function.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <regex>
+#include <sstream>
 using namespace std;
-vector <int> memory;
+
+const int MEMORY_LEN = 65536;
+int memory[MEMORY_LEN];
 
 int main()
 {
@@ -12,16 +15,32 @@ int main()
 	ifstream fin ("program.oa");
 	string command;
 
+	//command syntax templates
+	regex set("^SET \\d+ &\\d+$|^SET -\\d+ &\\d+$");
+
 	// main loop
-	while (!fin.eof())
+	//fetch in loop condition
+	while (getline(fin, command))
 	{
-		//fetch
-		getline(fin, command);
 		//execute
-		if (command.substr(0, 3) == "SET")
+		if (regex_match(command, set))
         {
-        	//remove "SET"
-		    command.erase(0, 4);
+			command.erase(0, 4);
+			stringstream value_str, adress_str;
+			int value, adress;
+			//slpit command to value and address
+			istringstream c_stream(command);
+			string s;
+			c_stream >> s;
+			value = stoi(s);
+			c_stream >> s;
+			//remove "&"
+			s.erase(0, 1);
+			adress = stoi(s);
+
+			if (adress > MEMORY_LEN-1)
+				cout << "Error: Cell in memory with this index doesn`t exist" << endl;
+			else memory[adress] = value;			
 		}
 	}
 
