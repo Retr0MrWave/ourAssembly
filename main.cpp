49fe5fc1e9b36bb50c6d5932ff08167e914aadf2
YaroslavQLT <yaroslav_ibragimov@ukr.net>
2019-12-20 20:24:49 +0000

    Added "ADD" command

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 const int MEMORY_LEN = 65536;
-int memory[MEMORY_LEN];
+int memory[MEMORY_LEN] = {0};
 
 vector<int> split(string& nums_str)
 {
@@ -33,25 +33,47 @@ int main()
 
 	//command syntax templates
 	regex set("^SET \\d+ &\\d+$|^SET -\\d+ &\\d+$");
+	regex add("^ADD \\d+ &\\d+$|^ADD -\\d+ &\\d+$");
 
 	// main loop
 	//fetch in loop condition
 	while (getline(fin, command))
 	{
-		//execute
+		//decode-execute
 		if (regex_match(command, set))
         {
+			//decode
 			command.erase(0, 4);
 			vector<int> args = split(command);
 			int value = args[0];
 			int adress = args[1];
-
+			//execute
 			if (adress > MEMORY_LEN-1)
 			{
 				cout << "IndexOutOfError: Cell in memory with this index doesn`t exist" << endl;
 				break;
 			}
-			else memory[adress] = value;			
+			else memory[adress] = value;
+		}
+		else if (regex_match(command, add))
+		{
+			//decode
+			command.erase(0, 4);
+			vector<int> args = split(command);
+			int value = args[0];
+			int adress = args[1];
+			//execute
+			if (adress > MEMORY_LEN-1)
+			{
+				cout << "IndexOutOfError: Cell in memory with this index doesn`t exist" << endl;
+				break;
+			}
+			else memory[adress] += value;
+		}
+		else
+		{
+			cout << "CompilationError: Undefined function:\"" + command + "\"";
+			break;
 		}
 	}
 
