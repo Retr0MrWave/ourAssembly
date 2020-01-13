75af1bba81565ec2e28c23a98e0f3c3159bef66e
YaroslavQLT <yaroslav_ibragimov@ukr.net>
2020-01-13 20:00:59 +0000

    Added regexes to MULTC, DIVC commands. Added implementation of PRINT command. Fixed some bugs.

--- main.cpp
+++ main.cpp
@@ -34,10 +34,14 @@ int main()
 	//command syntax templates
 	regex set("^SET \\d+ &\\d+$|^SET -\\d+ &\\d+$");
 	regex add("^ADD \\d+ &\\d+$|^ADD -\\d+ &\\d+$");
-	regex mult("^MULT \\d+ &\\d+$|^MULT -\\d+ &\\d+$");	regex div("^DIV \\d+ &\\d+$|^DIV -\\d+ &\\d+$");
-	regex add_cell("^ADD_CELL &\\d+ &\\d+$");
+	regex mult("^MULT \\d+ &\\d+$|^MULT -\\d+ &\\d+$");
+	regex div("^DIV \\d+ &\\d+$|^DIV -\\d+ &\\d+$");
+	regex addc("^ADDC &\\d+ &\\d+$");
+	regex setc("^SETC &\\d+ &\\d+$");
+	regex multc("^MULTC &\\d+ &\\d+$");
+	regex divc("^DIVC &\\d+ &\\d+$");
+	regex print("^PRINT &\\d+$");
 	
-
 	// main loop
 	//fetch in loop condition
 	while (getline(fin, command))
@@ -76,7 +80,7 @@ int main()
 		else if (regex_match(command, mult))
 		{
 			//decode
-			command.erase(0, 4);
+			command.erase(0, 5);
 			vector<int> args = split(command);
 			int value = args[0];
 			int adress = args[1];
@@ -102,6 +106,79 @@ int main()
 				break;
 			}
 			else memory[adress] /= value;
+		}
+		if (regex_match(command, setc))
+        {
+			//decode
+			command.erase(0, 5);
+			vector<int> args = split(command);
+			int value = args[0];
+			int adress = args[1];
+			//execute
+			if ((adress > MEMORY_LEN-1) || (value > MEMORY_LEN-1))
+			{
+				cout << "IndexOutOfError: Cell in memory with this index doesn`t exist" << endl;
+				break;
+			}
+			else memory[adress] = memory[value];
+		}
+		else if (regex_match(command, addc))
+		{
+			//decode
+			command.erase(0, 5);
+			vector<int> args = split(command);
+			int value = args[0];
+			int adress = args[1];
+			//execute
+			if ((adress > MEMORY_LEN-1) || (value > MEMORY_LEN-1))
+			{
+				cout << "IndexOutOfError: Cell in memory with this index doesn`t exist" << endl;
+				break;
+			}
+			else memory[adress] += memory[value];
+		}
+		else if (regex_match(command, multc))
+		{
+			//decode
+			command.erase(0, 6);
+			vector<int> args = split(command);
+			int value = args[0];
+			int adress = args[1];
+			//execute
+			if ((adress > MEMORY_LEN-1) || (value > MEMORY_LEN-1))
+			{
+				cout << "IndexOutOfError: Cell in memory with this index doesn`t exist" << endl;
+				break;
+			}
+			else memory[adress] *= memory[value];
+		}
+		else if (regex_match(command, divc))
+		{
+			//decode
+			command.erase(0, 5);
+			vector<int> args = split(command);
+			int value = args[0];
+			int adress = args[1];
+			//execute
+			if ((adress > MEMORY_LEN-1)||(value > MEMORY_LEN-1))
+			{
+				cout << "IndexOutOfError: Cell in memory with this index doesn`t exist" << endl;
+				break;
+			}
+			else memory[adress] /= memory[value];
+		}
+		else if(regex_match(command, print))
+		{
+			//decode
+			command.erase(0, 6);
+			vector<int> args = split(command);
+			int adress = args[0];
+			if ((adress > MEMORY_LEN-1))
+			{
+				cout << "IndexOutOfError: Cell in memory with this index doesn`t exist" << endl;
+				break;
+			}
+			else cout << memory[adress] << endl;	
 		}
 		else
 		{
